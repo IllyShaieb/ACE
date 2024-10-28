df18db95e70089890d9abef6b581d32f7d11e576
Illy Shaieb <shaiebilly@gmail.com>
2024-10-28 02:22:59 +0000

    Add process_user_input function and unit tests

--- ace/brain.py
+++ ace/brain.py
@@ -1,3 +1,14 @@
+import re
+
+
 def get_user_input() -> str:
     """Get user input."""
     return input("User: ")
+
+
+def process_user_input(user_input: str) -> str:
+    """Process user input."""
+    processed_input = user_input.lower()
+    processed_input = re.sub(r"[^\w\s]", "", processed_input)
+
+    return processed_input
--- tests/test_brain.py
+++ tests/test_brain.py
@@ -1,10 +1,16 @@
 import unittest
 from unittest.mock import patch
 
-from ace.brain import get_user_input
+from ace.brain import get_user_input, process_user_input
 
 
 class TestGetInput(unittest.TestCase):
     def test_get_user_input(self):
         with patch("builtins.input", return_value="This is a dummy user input."):
             assert get_user_input() == "This is a dummy user input."
+
+
+class TestProcessInput(unittest.TestCase):
+    def test_process_user_input(self):
+        processed = process_user_input("This is a dummy user input.")
+        assert processed == "this is a dummy user input"
