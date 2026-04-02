987cbc6f58ecf09987c0ffe2e93ef4c49cfaa807
Illy Shaieb <illy.shaieb@live.co.uk>
2026-04-02 14:30:57 +0100

    Rename adapters.services to adapters.http for better clarity

--- core/adapters/__init__.py
+++ core/adapters/__init__.py
@@ -1,8 +1,8 @@
 """core.adapters: This module defines adapters for the ACE application, providing
 a layer of abstraction for different APIs the tools might depend on."""
 
+from .http import RequestsHTTPAdapter
 from .io import BuiltinIOAdapter, RichIOAdapter
-from .services import RequestsHTTPAdapter
 from .storage import DatabaseConversationStorageAdapter, DatabaseLogStorageAdapter
 
 __all__ = [
